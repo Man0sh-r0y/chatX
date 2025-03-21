cf60a6f6a5882eadcaca50ce04c52923e2bc8d6f
Manash Roy <manash.roy.dev@gmail.com>
2025-03-22 02:27:41 +0530

    updated the file

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -5,12 +5,16 @@ export const generateToken = (userId, res) => {
     expiresIn: "7d",
   });
 
+  console.log("response: " + res);
+
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
     //sameSite: "strict", // CSRF attacks cross-site request forgery attacks
     //secure: process.env.NODE_ENV !== "development",
   });
+
+  console.log('cookie have created successfully');
 
   return token;
 };
