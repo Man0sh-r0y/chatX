8ed5634ff70540a4a108bb07ed606996ab7f6c11
Manash Roy <manash.roy.dev@gmail.com>
2025-03-22 14:51:58 +0530

    imported axios

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -4,6 +4,7 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 import { socketURL } from "../constants/index.js";
 import { baseURL } from "../constants/index.js";
+import axios from "axios";
 
 // const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 // const BASE_URL = "https://chatx-f80m.onrender.com";
