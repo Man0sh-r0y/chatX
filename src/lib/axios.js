a1c9a5b365dc36da684da49cddce0c81056d5b86
Manash Roy <manash.roy.dev@gmail.com>
2025-03-22 12:01:30 +0530

    updated the cors

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   // baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-  baseURL: "https://chatx-387m.onrender.com/api",
+  baseURL: "https://chatx-f80m.onrender.com/api",
   withCredentials: true,
 });
