820851962bfd6675b2461ec39a1343acbbd99fac
Manash Roy <manash.roy.dev@gmail.com>
2025-03-22 14:40:23 +0530

    token stored in localstorage after signup

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -37,6 +37,13 @@ export const useAuthStore = create((set, get) => ({
       const res = await axiosInstance.post("/auth/signup", data);
       set({ authUser: res.data });
       toast.success("Account created successfully");
+
+      const { token } = res?.data;
+      if(token) {
+        set({ token });
+        localStorage.setItem("user-token", token);
+      }
+      
       get().connectSocket();
     } catch (error) {
       toast.error(error.response.data.message);
