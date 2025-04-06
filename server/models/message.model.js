5849a60291ad069d6677a828da034b35a1a1666c
Manash Roy <manash.roy.dev@gmail.com>
2025-04-06 13:14:43 +0530

    added mongoose notes

--- server/models/message.model.js
+++ server/models/message.model.js
@@ -1,3 +1,8 @@
+/* The line `import mongoose from "mongoose";` is importing the Mongoose library in JavaScript.
+Mongoose is an Object Data Modeling (ODM) library for MongoDB and provides a higher-level
+abstraction for interacting with MongoDB databases. By importing Mongoose, you can use its
+functionalities to define schemas, models, and interact with MongoDB databases in your JavaScript
+code. */
 import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema(
