2b340592a614074abe27e888619384281a84fd0b
Manash Roy <manash.roy.dev@gmail.com>
2025-04-06 16:51:13 +0530

    updated the generateToken() function

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -1,17 +1,16 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
+
   try {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-      expiresIn: "7d",
+      expiresIn: "7d", // token will expire in 7 days
     });
   
-    console.log("response: " + res);
-  
     res.cookie("jwt", token, {
-      maxAge: 7 * 24 * 60 * 60 * 1000, // MS
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days represented in mili seconds
       httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-      //sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+      sameSite: "strict", // CSRF attacks cross-site request forgery attacks
       //secure: process.env.NODE_ENV !== "development",
     });
   
