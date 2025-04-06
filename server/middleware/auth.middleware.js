b03c1e31326bbb94c911ce081f45ba37e042fd51
Manash Roy <manash.roy.dev@gmail.com>
2025-04-06 15:16:53 +0530

    updated the middleware function

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -1,28 +1,29 @@
 import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 
+// work of middleware:
+
+// middleware to protect routes that require user authentication
+
+
 export const protectRoute = async (req, res, next) => {
   try {
-
-    console.log("Req body: ", req.body)
-
-    console.log("Req header: ", req.header("Authorization"))
     
-    const token = req.cookies.jwt || req.header("Authorization").replace("Bearer ", "") || req.body.token;
+    const token = req.cookies.jwt || req.header("Authorization").replace("Bearer ", "") || req.body.token; // finding the token from cookies or header or body
 
     if (!token) {
       return res.status(401).json({ message: "Unauthorized - No Token Provided (in middleware)" });
     }
 
-    console.log("Token found in middleware: ", token);
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET); // decode the token
 
     if (!decoded) {
       return res.status(401).json({ message: "Unauthorized - Invalid Token" });
     }
 
     const user = await User.findById(decoded.userId).select("-password");
+    // This tells Mongoose to exclude the password field from the result.
+    // The - sign before "password" means "don't include this field" in the returned document.
 
     if (!user) {
       return res.status(404).json({ message: "User not found" });
@@ -30,8 +31,14 @@ export const protectRoute = async (req, res, next) => {
 
     req.user = user;
 
+    /* `next();` is a function in Express.js middleware that is used to pass control to the next
+    middleware function in the stack. When `next()` is called within a middleware function, it tells
+    Express to move on to the next middleware in the chain. This is important for the flow of
+    control in Express applications, allowing multiple middleware functions to be executed in
+    sequence for a given route. */
     next();
-  } catch (error) {
+  } 
+  catch (error) {
     console.log("Error in protectRoute middleware: ", error.message);
     res.status(500).json({ message: "Internal server error" });
   }
