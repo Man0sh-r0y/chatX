3e15c40e97d0240dd03d5e686fef8390dcf4e572
Manash Roy <manash.roy.dev@gmail.com>
2025-04-06 16:43:58 +0530

    updated the socket.js file

--- server/lib/socket.js
+++ server/lib/socket.js
@@ -1,10 +1,11 @@
-import { Server } from "socket.io";
+import { Server } from "socket.io"; // Socket.IO is a JavaScript library that allows real-time, bi-directional communication between clients (like browsers) and servers.
 import http from "http";
 import express from "express";
 
 const app = express();
-const server = http.createServer(app);
+const server = http.createServer(app); // creating an HTTP server in Node.js that uses your Express app (app) to handle incoming requests.
 
+// creating a new Socket.IO server instance and binds it to existing HTTP server (server)
 const io = new Server(server, {
   cors: {
     origin: ["https://chatappbymanash.vercel.app"],
@@ -18,16 +19,20 @@ export function getReceiverSocketId(userId) {
 // used to store online users
 const userSocketMap = {}; // {userId: socketId}
 
-io.on("connection", (socket) => {
+io.on("connection", (socket) => { // it runs every time when a new user connects via Socket.IO
   console.log("A user connected", socket.id);
 
   const userId = socket.handshake.query.userId;
-  if (userId) userSocketMap[userId] = socket.id;
+
+  if (userId) userSocketMap[userId] = socket.id; // creating map of user IDs to their socket IDs
+  // It helps you to track who is online.
 
   // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  // io.emit() means broadcast to everyone.
+  // Objects.keys() returns an array of userIds.
 
-  socket.on("disconnect", () => {
+  socket.on("disconnect", () => { // It runs when that user closes the tab, loses internet, or disconnects.
     console.log("A user disconnected", socket.id);
     delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
