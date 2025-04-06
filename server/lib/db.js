fb20004fae08a51acd5fefe03ebe8d8f693e5c6d
Manash Roy <manash.roy.dev@gmail.com>
2025-04-06 15:20:44 +0530

    updated the db file

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,10 +1,13 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"; 
+// Mongoose is an Object Data Modeling (ODM) library for MongoDB and Node.js. 
+// Think of it as a tool that makes it easier to work with MongoDB in your JavaScript or Node.js applications.
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI); 
     console.log(`MongoDB connected: ${conn.connection.host}`);
-  } catch (error) {
+  } 
+  catch (error) {
     console.log("MongoDB connection error:", error);
   }
 };
